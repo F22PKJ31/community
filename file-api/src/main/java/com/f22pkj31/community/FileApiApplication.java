7b957b72dd2fb01b90b99e01c00a8efd6bd62a85
F22PKJ31 <1136286586@qq.com>
2019-03-14 00:07:30 +0800

    毕设

---
+++ file-api/src/main/java/com/f22pkj31/community/FileApiApplication.java
@@ -0,0 +1,15 @@
+package com.f22pkj31.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FileApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApiApplication.class, args);
+    }
+
+}
